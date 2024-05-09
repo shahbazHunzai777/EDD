e5cecbe5fdce8761db04b240b6945327c75dc3a7
shahbazHunzai777 <your.email@example.com>
2024-05-09 10:47:24 -0700

    Fix: Converted HTML File to xlsx

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,51 +1,40 @@
-const customPortalTemplate = '#CustomerPortalTemplate';
-const user = require('../fixtures/user.json');
+const urls = [
+  'https://uioPERFrf.edd.ca.gov/uioperfrf/Pages/Public/ExternalUser/UIOnlineLandingPage.aspx',
+  'https://sdioextpp1.edd.ca.gov/DIAExtPP1/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+  'https://uioSTGrf.edd.ca.gov/uioSTGrf/Pages/Public/ExternalUser/UIOnlineLandingPage.aspx',
+  'https://sdioextpp3.edd.ca.gov/DIAExtPP3/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+  'https://sdioextpp4.edd.ca.gov/DIAExtPP4/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+  'https://eservicespp.edd.ca.gov/eservicespp/Secure/RDS/BOSeServices/',
+  'https://uiouatRF.edd.ca.gov/uiouatrf/Pages/Public/ExternalUser/UIOnlineLandingPage.aspx',
+  'https://sdioextpp2.edd.ca.gov/DIAExtPP2/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+  'https://eservicest1.network1.corp.edd.ca.gov/eservicest1/Protected/RDS/BOSEnrollment/Landingpage/Login',
+];
 
-describe('Authentication flow', function () {
-  beforeEach(() => {
-    cy.fixture('user').as('user');
-  });
+urls.forEach(url => {
+  describe(`Login test for ${url}`, function () {
+    beforeEach(() => {
+      cy.fixture('user').as('user');
+    });
 
-  Cypress.on('uncaught:exception', (err, runnable) => {
-    return false;
-  });
-
-  it('Redirects to login', () => {
-    cy.visit('https://eddui--preprod2.sandbox.my.site.com/s/scplogin')
-      .wait(3000);
-
-    cy.url()
-      .should('include', '/s/scplogin');
-
-    cy.get(customPortalTemplate)
-      .find('.form-control.edd-input')
-      .eq(0)
-      .focus()
-      .type(user.username)
-      .should('have.value', user.username)
-      .trigger('input');
-
-    cy.get(customPortalTemplate)
-      .find('[name="Login password"]')
-      .should('have.value', '')
-      .type(user.password)
-      .should('have.value', user.password)
-      .trigger('input');
-
-    cy.get(customPortalTemplate)
-      .find('[type="submit"]')
-      .click()
-      .should('be.visible');
-
-    cy.url().should('include', '/s/');
-    cy.wait(3000);
-    cy.screenshot();
-
-    cy.get(customPortalTemplate)
-      .find('.card-body')
-      .find('button.edd-btn')
-      .first()
-      .click();
-    cy.screenshot();
+    it('Login test', () => {
+      cy.request({
+        method: 'GET',
+        url: url,
+        auth: {
+          'username': user.username,
+          'password': user.password
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        if (response.status === 200) {
+          cy.log('200 status code');
+        } else if (response.status === 401) {
+          cy.log('401 status code');
+        } else {
+          cy.log('Site is down');
+        }
+      });
+    });
   });
 });
+
