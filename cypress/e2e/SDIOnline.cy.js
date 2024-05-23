24819f312a8ea91c77a3047460486f3d6e5aece7
shahbazHunzai777 <your.email@example.com>
2024-05-23 03:09:41 -0700

    Fix: The commented files for eServices and UI-Online

--- cypress/e2e/SDIOnline.cy.js
+++ cypress/e2e/SDIOnline.cy.js
@@ -1,64 +1,64 @@
-// const customPortalTemplate = '#CustomerPortalTemplate';
-// const userObj = require('../fixtures/user.json');
+const customPortalTemplate = '#CustomerPortalTemplate';
+const userObj = require('../fixtures/user.json');
 
-// describe('Authentication flow', function () {
-//   beforeEach(() => {
-//     cy.fixture('user').as('user');
-//   });
+describe('Authentication flow', function () {
+  beforeEach(() => {
+    cy.fixture('user').as('user');
+  });
 
-//   Cypress.on('uncaught:exception', (err, runnable) => {
-//     return false;
-//   });
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
 
-//   const urls = [
-//     'https://sdioextpp1.edd.ca.gov/DIAExtPP1/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
-//     'https://sdioextpp3.edd.ca.gov/DIAExtPP3/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
-//     'https://sdioextpp4.edd.ca.gov/DIAExtPP4/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
-//     'https://sdioextpp2.edd.ca.gov/DIAExtPP2/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
-//   ];
+  const urls = [
+    'https://sdioextpp1.edd.ca.gov/DIAExtPP1/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+    'https://sdioextpp3.edd.ca.gov/DIAExtPP3/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+    'https://sdioextpp4.edd.ca.gov/DIAExtPP4/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+    'https://sdioextpp2.edd.ca.gov/DIAExtPP2/Pages/Public/ExternalUser/SDIOnlineLandingPage.aspx',
+  ];
 
-//   urls.forEach((url, index) => {
-//     it(`Visits ${url} and logs in`, function () {
-//       cy.visit(url)
-//         .wait(1000);
+  urls.forEach((url, index) => {
+    it(`Visits ${url} and logs in`, function () {
+      cy.visit(url)
+        .wait(1000);
 
-//       cy.get(customPortalTemplate)
-//         .find('.form-control.edd-input')
-//         .eq(0)
-//         .focus()
-//         .type(userObj.user1.username)
-//         .should('have.value', userObj.user1.username)
-//         .trigger('input');
+      cy.get(customPortalTemplate)
+        .find('.form-control.edd-input')
+        .eq(0)
+        .focus()
+        .type(userObj.user1.username)
+        .should('have.value', userObj.user1.username)
+        .trigger('input');
 
-//       cy.get(customPortalTemplate)
-//         .find('[name="Login password"]')
-//         .should('have.value', '')
-//         .type(userObj.user1.password)
-//         .should('have.value', userObj.user1.password)
-//         .trigger('input');
+      cy.get(customPortalTemplate)
+        .find('[name="Login password"]')
+        .should('have.value', '')
+        .type(userObj.user1.password)
+        .should('have.value', userObj.user1.password)
+        .trigger('input');
 
-//       cy.get(customPortalTemplate)
-//         .find('[type="submit"]')
-//         .click()
-//         .should('be.visible');
+      cy.get(customPortalTemplate)
+        .find('[type="submit"]')
+        .click()
+        .should('be.visible');
 
-//       cy.origin(
-//             url,
-//             () => {
-//               cy.get('#main_cmdClaimantRegistration').should('be.visible');
-//               cy.screenshot();
-//             })
+      cy.origin(
+            url,
+            () => {
+              cy.get('#main_cmdClaimantRegistration').should('be.visible');
+              cy.screenshot();
+            })
       
-//       cy.request({
-//         method: 'GET',
-//         url: url + '/rest/v1/',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       }).then(response => {
-//         expect(response.status).to.equal(200);
-//       });
-//     });
-//   });
-// });
+      cy.request({
+        method: 'GET',
+        url: url + '/rest/v1/',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        expect(response.status).to.equal(200);
+      });
+    });
+  });
+});
 
--- cypress/e2e/eServices.cy.js
+++ cypress/e2e/eServices.cy.js
@@ -1,52 +1,52 @@
-// const customPortalTemplate = '#CustomerPortalTemplate';
-// const userObj = require('../fixtures/user.json');
+const customPortalTemplate = '#CustomerPortalTemplate';
+const userObj = require('../fixtures/user.json');
 
-// describe('Authentication flow', function () {
-//   beforeEach(() => {
-//     cy.fixture('user').as('user');
-//   });
+describe('Authentication flow', function () {
+  beforeEach(() => {
+    cy.fixture('user').as('user');
+  });
 
-//   Cypress.on('uncaught:exception', (err, runnable) => {
-//     return false;
-//   });
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
 
-//   const urls = [
-//     'https://eservicespp.edd.ca.gov/eservicespp/Secure/RDS/BOSeServices/',
-//     'https://eservicest1.network1.corp.edd.ca.gov/eservicest1/Protected/RDS/BOSEnrollment/Landingpage'
-//   ];
+  const urls = [
+    'https://eservicespp.edd.ca.gov/eservicespp/Secure/RDS/BOSeServices/',
+    'https://eservicest1.network1.corp.edd.ca.gov/eservicest1/Protected/RDS/BOSEnrollment/Landingpage'
+  ];
 
-//   urls.forEach((url, index) => {
-//     it(`Visits ${url} and logs in`, function () {
-//       cy.visit(url)
-//         .wait(1000);
+  urls.forEach((url, index) => {
+    it(`Visits ${url} and logs in`, function () {
+      cy.visit(url)
+        .wait(1000);
 
-//       cy.get(customPortalTemplate)
-//         .find('.form-control.edd-input')
-//         .eq(0)
-//         .focus()
-//         .type(userObj.user1.username)
-//         .should('have.value', userObj.user1.username)
-//         .trigger('input');
+      cy.get(customPortalTemplate)
+        .find('.form-control.edd-input')
+        .eq(0)
+        .focus()
+        .type(userObj.user1.username)
+        .should('have.value', userObj.user1.username)
+        .trigger('input');
 
-//       cy.get(customPortalTemplate)
-//         .find('[name="Login password"]')
-//         .should('have.value', '')
-//         .type(userObj.user1.password)
-//         .should('have.value', userObj.user1.password)
-//         .trigger('input');
+      cy.get(customPortalTemplate)
+        .find('[name="Login password"]')
+        .should('have.value', '')
+        .type(userObj.user1.password)
+        .should('have.value', userObj.user1.password)
+        .trigger('input');
 
-//       cy.get(customPortalTemplate)
-//         .find('[type="submit"]')
-//         .click()
-//         .should('be.visible');
+      cy.get(customPortalTemplate)
+        .find('[type="submit"]')
+        .click()
+        .should('be.visible');
 
-//       cy.origin(
-//             url,
-//             () => {
-//               cy.get('#action_6').should('be.visible');
-//               cy.screenshot();
-//             })
-//     });
-//   });
-// });
+      cy.origin(
+            url,
+            () => {
+              cy.get('#action_6').should('be.visible');
+              cy.screenshot();
+            })
+    });
+  });
+});
 
